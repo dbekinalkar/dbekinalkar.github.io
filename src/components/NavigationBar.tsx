fcfefd1f4398da7b5d6b67117f4db227c31694f2
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-01-11 22:13:28 -0500

    Fix typo for menu visibility

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -31,7 +31,7 @@ const NavigationBar = ({
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             {children}
           </ul>
